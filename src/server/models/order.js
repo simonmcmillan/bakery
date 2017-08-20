27529e8bfc68b585dd68679f5469863da32aa44f
Simon McMillan <simon.p.mcmillan@gmail.com>
2017-08-20 20:05:27 +1000

    added nodemon debugger

--- src/server/models/order.js
+++ src/server/models/order.js
@@ -1,6 +1,7 @@
 import products from './products';
 
 export default class Order {
+
 	handleOrder (order) {
 		return order.reduce((acc, item) => {
 			if (!item.id && item.amount) {
@@ -8,7 +9,7 @@ export default class Order {
 			}
 
 			const minPacks = this.calculateMinPacks(this.getPackAmounts(products[item.id]), item.amount);
-			console.log('exit', minPacks);
+			console.log('MIN PACKS', minPacks);
 
 			acc.push(minPacks);
 		}, []);
@@ -17,14 +18,17 @@ export default class Order {
 	getPackAmounts (product) {
 		return product.packs.map(pack => pack.amount);
 	}
-	//**Get the minimun that add to order amount**
-	//1. Sort pack size from largest to smallest
-	//2. recursive loop, check exit condition: orderAmount matches sum pack sizes, break.
-	//3. push largest pack size less than order to final array
-	//4. take difference between pack size and order
-	//5. add largest pack size less than remainder to the pushed packs, if greater than total pop pushed pack (store the used pack size value)
-	//6. push second largest pack size to final order
-	//7. repeat
+
+	rewind (packs, remainder, finalOrder) {
+		//pop the last pack from the finalOrder and add the pack amount back to remainder
+		const popped = finalOrder.pop();
+		remainder = remainder + popped;
+		//unshift the first element off the packs array
+		packs.shift();
+
+		return remainder;
+	}
+
 	calculateMinPacks (packs, orderAmount, remainder = orderAmount, finalOrder = []) {
 		let finalOrderSum;
 
@@ -34,23 +38,19 @@ export default class Order {
 		if (pack) {
 			finalOrder.push(pack);
 			finalOrderSum = this.sumOrder(finalOrder);
+			debugger;
+
+			if (finalOrderSum === orderAmount || finalOrder.length > 5) {
+				return finalOrder;
+			}
+			// get the remaining amount on the order
+			else {
+				remainder = remainder - pack;
+			}
 		}
-console.log(finalOrder);
 		//handle if we cant match the order
-		if (!pack || finalOrder.length > 5) {
-			//pop the last pack from the finalOrder and add the pack amount back to remainder
-			const popped = finalOrder.pop();
-			remainder = remainder + popped;
-			//unshift the first element off the packs array
-			packs.shift();
-		}
-		//exit the loop if we have matched the order
-		else if (finalOrderSum === orderAmount || finalOrder.length > 5) {
-			return finalOrder;
-		}
-		// get the remaining amount on the order
 		else {
-			remainder = remainder - pack;
+			remainder = this.rewind(packs, remainder, finalOrder);
 		}
 
 		return this.calculateMinPacks(packs, orderAmount, remainder, finalOrder);
