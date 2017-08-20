f4db90a919b0ae2aea75badddb25c8c901e70078
Simon McMillan <simon.p.mcmillan@gmail.com>
2017-08-21 07:45:48 +1000

    f

--- src/server/models/order.js
+++ src/server/models/order.js
@@ -9,7 +9,7 @@ export default class Order {
 			}
 
 			const minPacks = this.calculateMinPacks(this.getPackAmounts(products[item.id]), item.amount);
-			console.log('MIN PACKS', minPacks);
+			// console.log('MIN PACKS', minPacks);
 
 			acc.push(minPacks);
 		}, []);
@@ -19,28 +19,32 @@ export default class Order {
 		return product.packs.map(pack => pack.amount);
 	}
 
-	rewind (packs, remainder, finalOrder) {
+	rewind (packs, remainder, finalOrder, popAmount) {
+		let popped;
+		// debugger;
 		//pop the last pack from the finalOrder and add the pack amount back to remainder
-		const popped = finalOrder.pop();
-		remainder = remainder + popped;
+		for (let i = 0;  i < popAmount; i++) {
+			popped = finalOrder.pop();
+			remainder = remainder + popped;
+		}
 		//unshift the first element off the packs array
 		packs.shift();
 
 		return remainder;
 	}
 
-	calculateMinPacks (packs, orderAmount, remainder = orderAmount, finalOrder = []) {
+	calculateMinPacks (packs, orderAmount, remainder = orderAmount, finalOrder = [], popAmount = 1) {
 		let finalOrderSum;
 
 		// get the first pack smaller than whats left in remainder of order
 		const pack = packs.find((pack) => pack <= remainder);
 		// push the pack on the stack
-		if (pack) {
+		if (pack !== undefined) {
 			finalOrder.push(pack);
 			finalOrderSum = this.sumOrder(finalOrder);
-			debugger;
+			// debugger;
 
-			if (finalOrderSum === orderAmount || finalOrder.length > 5) {
+			if (finalOrderSum === orderAmount) {
 				return finalOrder;
 			}
 			// get the remaining amount on the order
@@ -50,10 +54,14 @@ export default class Order {
 		}
 		//handle if we cant match the order
 		else {
-			remainder = this.rewind(packs, remainder, finalOrder);
+			// if at the last pack pop an extra pack from the finalOrder
+			if (packs.indexOf(finalOrder[finalOrder.length - 1]) === packs.length - 1) {
+				popAmount++;
+			}
+			remainder = this.rewind(packs, remainder, finalOrder, popAmount);
 		}
 
-		return this.calculateMinPacks(packs, orderAmount, remainder, finalOrder);
+		return this.calculateMinPacks(packs, orderAmount, remainder, finalOrder, popAmount);
 	}
 
 	sumOrder (arr) {
